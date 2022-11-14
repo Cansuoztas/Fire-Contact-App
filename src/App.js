3225ce6fa0aecd28ece502e718c901acb8243c42
Cansu Öztaş <fcansuoztas@gmail.com>
2022-11-15 00:00:54 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
+import "./App.css";
+import Contacts from "./components/contacts/Contacts";
+import FormComponent from "./components/form/FormComponent";
+import {useState} from "react"
+import {AddUser,UpdateUser} from "./utils/functions"
+import { ToastContainer } from "react-toastify";
 
-import './App.css';
-// import { ToastContainer } from "react-toastify";
-import Table from './components/table/Table';
-import Form from './components/form/Form';
-
+const initialValues={
+  username:"",
+  phoneNumber:"",
+  gender:""
+}
 function App() {
+  const [info, setInfo] = useState(initialValues)
+  const [isAdd,setIsAdd]=useState("ADD")
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(info.id){
+      UpdateUser(info)
+    }
+    else{
+      AddUser(info)
+    }
+    setInfo(initialValues)
+    setIsAdd("ADD")
+  }
+
+  const editUser=(id,username,phoneNumber,gender)=>{
+    setIsAdd("UPDATE")
+    setInfo({id,username,phoneNumber,gender})
+  }
+
   return (
-    <div className="App"> 
-      <Form/>
-      <Table/>
-   
-      {/* <ToastContainer/>
-       */}
+    <div className="App">
+      <FormComponent info={info} setInfo={setInfo} handleSubmit={handleSubmit} isAdd={isAdd} />
+      <Contacts editUser={editUser}/>
+      <ToastContainer/>
     </div>
   );
 }
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,22 +1,15 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBYpO1sW9uZdOa3uCeFUZBNl-trc3pS86o",
-  authDomain: "fire-contact-app-a5da4.firebaseapp.com",
-  databaseURL: "https://fire-contact-app-a5da4-default-rtdb.firebaseio.com",
-  projectId: "fire-contact-app-a5da4",
-  storageBucket: "fire-contact-app-a5da4.appspot.com",
-  messagingSenderId: "551554735441",
-  appId: "1:551554735441:web:31d270f6bc848786ac0770",
-  measurementId: "G-TZ65HHZZJ5"
+  apiKey: "AIzaSyBCn0rVXGkt6RKfe3pn0tY6GoYtGOVKdhE",
+  authDomain: "fire-contact-project-20f9f.firebaseapp.com",
+  databaseURL: "https://fire-contact-project-20f9f-default-rtdb.firebaseio.com",
+  projectId: "fire-contact-project-20f9f",
+  storageBucket: "fire-contact-project-20f9f.appspot.com",
+  messagingSenderId: "123967912053",
+  appId: "1:123967912053:web:a065b63d59a03bb139a03c"
 };
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
