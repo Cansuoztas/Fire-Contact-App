c124851b2c6b4aa8aa232c65db85a75f4b447096
Cansu Öztaş <fcansuoztas@gmail.com>
2022-11-15 00:00:38 +0300

    edit

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,15 +1,63 @@
-import firebase from './firebase'
-import { getDatabase,ref, push, set} from 'firebase/database'
+// ADD USER
+
+import { getDatabase, ref, set,push, onValue, remove,update } from "firebase/database";
+import firebase from "./firebase"
+import {useState,useEffect} from "react"
+import Toastify from "./toastify";
 
 
-export const AddUser =(name,phone,gender)=>{
-    const db = getDatabase();
-    const userRef = ref (db,'baglanti');
-    const newUserRef = push(userRef);
-    set((newUserRef),{
-        userName:name,
-        phoneNumber:phone,
-        gender:gender
+export const AddUser=(info)=>{
+    const db=getDatabase(firebase)
+    const userRef=ref(db,"user/")
+    const newUserRef=push(userRef)
+
+    set(newUserRef,{
+        username:info.username,
+        phoneNumber:info.phoneNumber,
+        gender:info.gender
     })
+  
+}
+
+// READ INFO
+export const useFetch=()=>{
+    const [isLoading,setIsLoading]=useState(true)
+    const [contactList,setContactList]= useState()
+    useEffect(() => {
+        const db=getDatabase(firebase)
+        const userRef=ref(db,"user/")
+
+        onValue(userRef,(snapshot)=>{
+            const data = snapshot.val();
+            const userArray=[]
+
+            for(let id in data){
+                userArray.push({id,...data[id]})
+            }
+            setContactList(userArray)
+            setIsLoading(false)
+        })
+       
+    },[])
+    return {isLoading,contactList}
+
+}
+
+export const DeleteUser=(id)=>{
+    const db = getDatabase(firebase);
+    remove(ref(db,"user/"+id))
+    Toastify("Deleted Successfully")
+
+}
+
+export const UpdateUser=(info)=>{
+    const db=getDatabase(firebase)
+    const userRef=ref(db,"user/")
+
+    const updates = {};
+
+    updates["user/"+info.id]=info
+
+    return update(ref(db), updates);
 
 }
